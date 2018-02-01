7597bf2ed34836af85452882d59d4288e8d09ecb
delgee <stormrage0527@gmail.com>
2018-02-01 15:59:43 +0800

    removed test

--- tests/index-test.js
+++ tests/index-test.js
@@ -2,8 +2,6 @@ import expect from 'expect'
 import React from 'react'
 import {render, unmountComponentAtNode} from 'react-dom'
 
-import Component from 'src/'
-
 describe('Component', () => {
   let node
 
